ba1e81111914b808ca2c41f9407e6c0650ee4cd4
Eric Yeung <heihei0314@gmail.com>
2025-07-16 12:24:52 +0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	deleted:    guide/admin.py
    	deleted:    guide/models.py
    	deleted:    guide/static/guide/css/style.css
    	deleted:    guide/static/guide/js/script.js
    	deleted:    guide/templates/guide/proposal_guide.html
    	deleted:    guide/urls.py
    	deleted:    guide/views.py
    	deleted:    proposal_builder/urls.py
    	deleted:    readme.md
    
     Untracked files:
    	.gitignore
    	api.php
    	assets/
    	content note.txt
    	css/
    	flask/
    	index.html
    	js/
    	prompt reference/
    	py/
    	readme.md

--- guide/admin.py
+++
@@ -1,8 +0,0 @@
-# guide/admin.py
-from django.contrib import admin
-from .models import ProposalStep
-
-@admin.register(ProposalStep)
-class ProposalStepAdmin(admin.ModelAdmin):
-    list_display = ('title', 'order')
-    list_editable = ('order',)
--- proposal_builder/urls.py
+++
@@ -1,8 +0,0 @@
-# proposal_builder/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('guide.urls')), # Include your app's URLs
-]
